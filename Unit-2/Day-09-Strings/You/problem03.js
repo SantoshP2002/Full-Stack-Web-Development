ce135deebbcce6086d3cc8a4de423e2c9e7f98f4
SantoshP2002 <santoshpawar96k@gmail.com>
2024-04-16 19:41:15 +0530

    Added

--- Unit-2/Day-09-Strings/You/problem03.js
+++ Unit-2/Day-09-Strings/You/problem03.js
@@ -1,12 +1,12 @@
-let arry = [1,2,3,4,5,6,7];
+let arr = [1,2,3,4,5,6];
 
 // even: 3 Or odd: 3;
 
 let even_count = 0;
 let odd_count = 0;
 
-for(let i=0; i<=arry.length; i++){
-    if(i%2==0){
+for(let i=0; i<arr.length; i++){
+    if(arr[i]%2==0){
         even_count++;
     }
     else{
@@ -14,4 +14,3 @@ for(let i=0; i<=arry.length; i++){
     }
 }
 console.log('even:',even_count,'odd:',odd_count);
-
