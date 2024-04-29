5e50c08ad86ac85531e270a9f5e96f87be185cce
SantoshP2002 <santoshpawar96k@gmail.com>
2024-04-30 01:03:26 +0530

    Added

---
+++ Unit-2/Day-11-Object-I/You/problem06.js
@@ -0,0 +1,27 @@
+let details = {
+    name: "santosh",
+    father_name: "shivajirao",
+    mother_name: "gokarna",
+    age: 22,
+    city: "nanded",
+    pincode: 431606,
+    
+  };
+
+  // Update the already present data;
+
+  details.age = 30;
+  details.city = "Amdura";
+//   console.log(details);
+
+  let obj2= {}
+
+  obj2.name = 'santosh';
+  obj2.name = 'pavan';
+//   console.log(obj2);
+
+  let obj3 = {
+    age: 21,
+    age: 23,
+  }
+console.log(obj3);
