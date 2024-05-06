ae0a212db8782625c114030b969debeb71b6e5c9
SantoshP2002 <santoshpawar96k@gmail.com>
2024-05-06 20:39:29 +0530

    Added

--- Unit-2/Day-16-HOF/problem03.js
+++ Unit-2/Day-16-HOF/problem03.js
@@ -1,13 +1,13 @@
-let array = [1,2,3,4,5,6];
 
 // Output ---> [1,4,9,16,25,36];
 
 function print(arr,number){
     output = [];
-    for(let i=0; i<array.length; i++){
-        output.push(array[i]**number);
+    for(let i=0; i<arr.length; i++){
+        output.push(arr[i]**number);
     }
     console.log(output);
 }
 
+let array = [1,2,3,4,5,6];
 print(array,2)
---
+++ Unit-2/Day-16-HOF/problem05.js
@@ -0,0 +1,35 @@
+//  let array = [12,34,56,79,85]
+
+// let result =  array.map(function(num){
+//     return num**2;
+//  })
+
+//  console.log(result);
+
+
+// // Original array
+// let numbers = [1, 2, 3, 4, 5];
+
+// // map() ka use karke har element ko double karke naya array banate hain
+// let doubledNumbers = numbers.map(function(num) {
+//   return num * 2;
+// });
+
+// console.log(doubledNumbers); // Output: [2, 4, 6, 8, 10]
+
+// let array = [1,2,3,4,5,6,7,9];
+
+// let result = array.map(function(num){
+//   return num + 2;
+// })
+// console.log(result);
+
+
+let array = [1,2,3,4,56,6,78,];
+
+let result = array.map(function(value){
+  return value - 10;
+})
+console.log(result);
+
+// Output = > [-9,-8,-7,-6,46,-4,68];
