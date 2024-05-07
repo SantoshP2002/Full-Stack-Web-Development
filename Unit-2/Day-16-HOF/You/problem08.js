863f051ff69eb8038370b42281d96ca295b18e97
SantoshP2002 <santoshpawar96k@gmail.com>
2024-05-07 17:07:22 +0530

    Basic javascript Completed

---
+++ Unit-2/Day-16-HOF/You/problem08.js
@@ -0,0 +1,11 @@
+
+// forEach Method: -->
+// Syntax
+// arrayName.forEach((element, Index)=>{  code logic  });
+
+
+let array = [1,2,3,4,5,6,7];
+
+array.forEach(function(num){
+    console.log(num +2);
+})
