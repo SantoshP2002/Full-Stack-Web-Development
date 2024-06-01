99c1bfaa7bbed2d0a9d86c777329df785ebde367
SantoshP2002 <santoshpawar96k@gmail.com>
2024-06-01 19:28:49 +0530

    Added

---
+++ Unit-4/Day-03-State Management Techniques and useState Hook/You/session1/index.js
@@ -0,0 +1,15 @@
+// Declarative and Imperative Programming =>
+
+    // Declarative :
+    // <h1 id="container">Hello World !</h1>
+
+    // imperative :
+    // let container = document.getElementById('container');
+    // let box = document.createElement('div');
+
+    // box.textContent =  'Hello World';
+
+    // container.append(box);
+
+
+    
