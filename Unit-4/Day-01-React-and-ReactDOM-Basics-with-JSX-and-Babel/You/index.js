e6ce15e1693acb822198cb6f664cb5ecfe21a8ec
SantoshP2002 <santoshpawar96k@gmail.com>
2024-06-15 17:00:49 +0530

    JSX and Babel completed

---
+++ Unit-4/Day-01-React-and-ReactDOM-Basics-with-JSX-and-Babel/You/index.js
@@ -0,0 +1,47 @@
+// let root = document.getElementById("root");
+
+// let create = document.createElement("div")
+// create.textContent = "Hello"
+// console.log(create);>
+// root.append(create)
+
+// React =>
+
+// const element = React.createElement("div",{className : "container"},
+//                 React.createElement("h1",{className: "box1"}, "Hello"),
+//                 React.createElement("h2", {className: "box2"}, "Hello1"))
+
+
+
+
+
+// InterPolation =>
+
+// let data = true;
+// const element = <div className="container" style={{color:data? "red": "green"}}>
+//                 /* <p className="box1">{data?"santosh": "shrutika"}</p>
+//                 <p className="box2">pawar</p> */
+//                 </div>;
+
+// const reactRoot = ReactDOM.createRoot(document.getElementById("root"));
+// reactRoot.render(element);
+
+
+
+
+// Event =>
+
+// function alertBox(a) {
+//   console.log("hello" + a);
+// }
+
+// let data = true;
+// const element = (<div className="container" style={{ color: data ? "red" : "green" }}>
+//     {data ? 
+//     ( <button onClick={() => alertBox("santosh")}>Login</button> ) :
+//      ( <button>Logout</button> )}
+//   </div>
+// );
+
+// const reactRoot = ReactDOM.createRoot(document.getElementById("root"));
+// reactRoot.render(element);
