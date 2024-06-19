96bf98224baf70bb1b01630363db441cb5970381
SantoshP2002 <santoshpawar96k@gmail.com>
2024-06-19 07:11:13 +0530

    useState Completed

--- Unit-4/Day-03-State Management Techniques and useState Hook/You/index.js
+++ Unit-4/Day-03-State Management Techniques and useState Hook/You/index.js
@@ -28,8 +28,15 @@
 
 // }
 
-function Navbar() {
-  return <div>I am Navbar</div>;
+function Navbar({ data, setData }) {
+  let a = 9;
+  return (
+    <div>
+      {data} is navBar
+      {/* <button onClick={() => setData(a)}>Increase</button> */}
+      <input onChange={(e)=>setData(e.target.value)}/>
+    </div>
+  );
 }
 
 function Footer() {
@@ -46,39 +53,115 @@ function Footer() {
 //   );
 // }
 
-function App() {
-  const [val, setVal] = React.useState("");
-  const [classes, setClasses] = React.useState("");
-  const [age, setAge] = React.useState("");
-  function increaseVal(e) {
-    setVal(e.target.value);
+// function Card(name, age, classes){
+//   return (
+//     <div>
+//       <p>Name :-{name}</p>
+//     </div>
+//   )
+// }
 
-    // setVal(val+1)
-    // console.log(val+1);
-  }
+function App() {
+  // const [val, setVal] = React.useState("");
+  // const [age, setAge] = React.useState("");
+  // const [classes, setClasses] = React.useState("");
+
+  // const [users, setUsers] = React.useState([]);
+  const [data, setData] = React.useState("");
+  // name: "",
+  // age: "",
+  // classes: "",
+  // isMarried: false,
+
+  // function increaseVal(e) {
+  //   setVal(e.target.value);
+
+  //   // setVal(val+1)
+  //   // console.log(val+1);
+  // }
 
   // function decreaseVal(){
   //   setVal(val-1)
   //   console.log(val-1);
   // }
 
-  function submitVal() {
-    console.log(val);
-    console.log(age);
-    console.log(classes);
-    setVal(""); 
-  }
+  // function submitVal() {
+  //   console.log(val);
+  //   console.log(age);
+  //   console.log(classes);
+  //   setVal("");
+  //   setAge("");
+  //   setClasses("");
+  // }
+  // const handleChangeInput = (e) => {
+  //   // console.log("PrevData", data);
+  //   const { name, value, type } = e.target;
+  //   setData((prevData) => {
+  //     return {
+  //       ...prevData, // {name:"Santosh",age:"",classes:""}
+  //       [name]: type == "number" ? +value : type == "checkbox" ? true : value,
+  //     };
+  //   });
+  // };
+  // const handleSubmit = () => {
+  //   console.log("Data", data);
+  //   setUsers((prevUsers) => {
+  //     return [...prevUsers, data];
+  //   });
+  // };
+  // console.log("Users", users);
 
   return (
     <div>
-      <Navbar />
-      {val} <br />
-      <input type="text" value={val} onChange={(e) =>setVal(e.target.value)} />
-      <input type="text" value={age} onChange={(e) =>setAge(e.target.value)} />
-      <input type="text" value={classes} onChange={(e) =>setClasses(e.target.value)} />
-      <br />
-      <button onClick={submitVal}>increase</button>
+      {/* <Navbar  val= {data}/> */}
+      <Navbar data={data} setData={setData} />
+      {/* {data} */}
+      {/* {val} <br />
+      <input type="text" value={val} onChange={(e) => setVal(e.target.value)} />
+      <input type="text" value={age} onChange={(e) => setAge(e.target.value)} />
+      <input
+        type="text"
+        value={classes}
+        onChange={(e) => setClasses(e.target.value)}
+      />
+      <br /> */}
+      {/* <button onClick={submitVal}>increase</button> */}
       {/* <button onClick={decreaseVal}>decrease</button> */}
+      <input
+        type="text"
+        value={data}
+        onChange={(e) => setData(e.target.value)}
+      />
+      {/* <input
+        type="text"
+        name="name"
+        placeholder="Enter Name"
+        onChange={handleChangeInput}
+      /> */}
+      {/* <input
+        type="number"
+        name="age"
+        placeholder="Enter Age"
+        onChange={handleChangeInput}
+      />
+      <input
+        type="checkbox"
+        name="isMarried"
+        value = {data.isMarried}
+        placeholder="Enter Age"
+        onChange={handleChangeInput}
+      />
+      <input
+        type="text"
+        name="classes"
+        placeholder="Enter Classes"
+        onChange={handleChangeInput}
+      /> */}
+      <br />
+      {/* <button onClick={handleSubmit}>Submit</button>
+      <pre>
+        <code>{JSON.stringify(users, null, 2)}</code>
+      </pre> */}
       <Footer />
     </div>
   );
