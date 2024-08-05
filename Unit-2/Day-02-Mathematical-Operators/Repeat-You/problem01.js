bae61d725625de72d4626dbec54deadf30a5a98f
SantoshP2002 <santoshpawar96k@gmail.com>
2024-08-05 13:48:40 +0530

    Added

---
+++ Unit-2/Day-02-Mathematical-Operators/Repeat-You/problem01.js
@@ -0,0 +1,28 @@
+// let a =10;
+// let b = 5;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+
+// console.log(1+2);
+
+// console.log(10%2); // 0
+// console.log(7%2);
+// console.log(6%123);
+
+// console.log("hello world", "how are you");
+
+let a = 2;
+let b = 6;
+let c = 5;
+let d = "pavan"
+
+
+console.log(typeof(a+b));
+console.log(typeof(a+d));
+
+// console.log(a**b);
+
+// console.log("hii"+"  " +"santosh");
---
+++ Unit-2/Day-03-Relational-And-Comparison-Operators/Repeat-You/problem01.js
@@ -0,0 +1,9 @@
+let a = "goku"
+let b =  "goku"
+
+
+// double equal to(==)
+// console.log(a==b);
+
+// not equal to (!=)
+console.log(a!=b);
